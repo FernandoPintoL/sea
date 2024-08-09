7ad4ab874f7cc7148f658ccf7ab36c8e21ef8caa
Fernando Pinto Lino <pintolinofernando@gmail.com>
2024-08-09 16:42:24 -0400

    galeria visitante optimizado.

--- app/Http/Controllers/GaleriaVisitanteController.php
+++ app/Http/Controllers/GaleriaVisitanteController.php
@@ -34,9 +34,9 @@ class GaleriaVisitanteController extends Controller
                 $file = $request->file( 'file' );
                 $extension = $file->getClientOriginalExtension();
                 $filename= "cod".$model->id."-visitanteid".$request->get("id").'.'.$extension;
-                $path = $file->storeAs('visitantes', $filename, 'public');
+                // $path = $file->storeAs('visitantes', $filename, 'public');
                 //$path = Storage::putFileAs('/visitantes', $request->file('file'), $filename);
-                //$path = Storage::disk('s3')->put('visitantes', $file);
+                $path = Storage::disk('s3')->put('visitantes', $file);
                 
                 /*ACTUALIZA LA TABLA CON LA RUTA*/
                 $url = Storage::url($path);
