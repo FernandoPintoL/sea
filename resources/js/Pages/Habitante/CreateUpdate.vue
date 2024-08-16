c7744bc7d5d52b721e76bb1bdc239f0befc0763a
Fernando Pinto Lino <pintolinofernando@gmail.com>
2024-08-15 23:17:25 -0400

    habitante.

--- resources/js/Pages/Habitante/CreateUpdate.vue
+++ resources/js/Pages/Habitante/CreateUpdate.vue
@@ -448,7 +448,6 @@ const fecha = (fechaData) => {
 
             <select
               id="countries"
-              required
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               v-model="form.perfil.tipo_documento_id"
             >
@@ -478,7 +477,6 @@ const fecha = (fechaData) => {
               <input
                 v-model="form.perfil.nroDocumento"
                 @input="onValidateNroDocumento"
-                required
                 type="text"
                 id="nroDocumento"
                 class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -599,7 +597,6 @@ const fecha = (fechaData) => {
               <div class="mr-3">
                 <select
                   id="viviendas"
-                  required
                   class="col-span-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   v-model="form.vivienda_id"
                 >
